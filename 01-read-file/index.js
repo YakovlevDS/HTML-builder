96a8b9002a4b3aa11a40602fcebcd6a6f5d7da15
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-17 08:36:29 +0300

    :refactor: fix and format 02-write-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const {stdout} = process;
+const { stdout } = process;
 const nameTargetFile = 'text.txt';
 const pathToFile = path.join(__dirname, nameTargetFile);
 const stream = fs.createReadStream(pathToFile);
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
-const path = require("path");
-const {stdin, stdout, exit} = process;
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout, exit } = process;
 
-const fileName ="text.txt"
-const pathToFile =path.join(__dirname, fileName);
+const fileName = 'text.txt';
+const pathToFile = path.join(__dirname, fileName);
 let stream = fs.createWriteStream(pathToFile);
 
-const exitPhrase=()=> {
-    stdout.write("\n Goodbye!");
-    exit();
-  }
+const exitPhrase = () => {
+  stdout.write('\n Goodbye!');
+  exit();
+};
 
-stdout.write("Hello please enter below the text for writing it to the file or 'exit' for EXIT: \n");
+stdout.write('Hello please enter below the text for writing it to the file or "exit" for EXIT: \n');
 
-stdin.on("data", str => 
-  (''+ str).trim()==="exit" ? exitPhrase() : stream.write(str));
+stdin.on('data', str =>
+  ('' + str).trim() === 'exit' ? exitPhrase() : stream.write(str));
 
 process.on('SIGINT', exitPhrase);
 
