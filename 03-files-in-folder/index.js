cb7d1a9ec48c6f3d3d910f226bc764a74119904a
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-17 08:45:52 +0300

    refactor: add consts

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,28 +1,36 @@
 
 const fs = require('fs');
 const path = require('path');
-const {stdout } = process;
+const { stdout } = process;
 
-let folderName='secret-folder';
-let dirPath= path.join(__dirname, folderName);
+let folderName = 'secret-folder';
+let dirPath = path.join(__dirname, folderName);
 
-const infoFiles = (file)=> {
-    const pathFile=path.resolve(__dirname, folderName, file.name);
+const infoFiles = (file) => {
+  const pathFile = path.resolve(__dirname, folderName, file.name);
 
-    fs.stat(pathFile,  (err, status)=> {
+  fs.stat(pathFile, (err, status) => {
     if (err) throw err;
-     const nameFile=path.basename(file.name);
-    const sizeFile=`${Math.round(status.size/1024)}Kb`;
-    const createFile=status.birthtime;
-    const dateCreate=`${createFile.getDate()}.${String(createFile.getMonth() + 1)}.${createFile.getFullYear()} ${createFile.getHours()}:${createFile.getMinutes()}`
-     stdout.write(`${nameFile} size:${sizeFile} created: ${dateCreate}\n`);
-    });
-  }
+    const nameFile = path.basename(file.name);
+    const sizeFile = `${Math.round(status.size / 1024)}Kb`;
+    const createFile = status.birthtime;
+    const date = createFile.getDate();
+    const month = String(createFile.getMonth() + 1);
+    const year = createFile.getFullYear();
+    const hours = createFile.getHours();
+    const minutes = createFile.getMinutes();
+    const dateCreate = `${date}.${month}.${year} ${hours}:${minutes}`;
+    stdout.write(`${nameFile} size:${sizeFile} created: ${dateCreate}\n`);
+  });
+};
 
-const  itemIsFile =()=>{
-    fs.readdir(dirPath, { withFileTypes: true },(err, files)=> {
-        if (err) throw err;
-        files.forEach(i => {if (i.isFile()) infoFiles(i)});
-})};
+const itemIsFile = () => {
+  fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    files.forEach(i => {
+      if (i.isFile()) infoFiles(i);
+    });
+  });
+};
 itemIsFile();
 
